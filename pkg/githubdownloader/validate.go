9fa8f61a8f4c2a5d50788668d3722e1e52a8d3e8
dviejokfs <dviejo@kfs.es>
2025-05-27 17:10:20 +0200

    Update it
    Signed-off-by: dviejokfs <dviejo@kfs.es>

--- pkg/githubdownloader/validate.go
+++ pkg/githubdownloader/validate.go
@@ -12,7 +12,7 @@ var (
 // ValidateGitHubURL checks if the provided URL is a valid GitHub repository URL.
 func ValidateGitHubURL(url string) error {
 	// Basic regex for GitHub repo URLs (https only)
-	re := regexp.MustCompile(`^https://github.com/[^/]+/[^/]+/?$`)
+	re := regexp.MustCompile(`^https://github\.com/[^/]+/[^/]+/?$`)
 	if !re.MatchString(url) {
 		return ErrInvalidGitHubURL
 	}
